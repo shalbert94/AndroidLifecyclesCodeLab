d0d50ceca23a63f5ed8fadb26f9bd7cf747b4a75
Shalom Halbert <shalomhalbert@gmail.com>
2018-05-01 18:28:27 -0400

    Added ChoronometerViewModel to ViewModelStore; Utilize ViewModel
    - ChoronometerViewModel is initialized and added to ViewModelStore
    - Benefiting from the ViewModel's data persistence, an if-else statement
    handle a case for before and after a start time is set through
    viewModel.init()

--- app/src/main/java/com/example/shalom/androidlifecyclescodelab/ChronoActivity.java
+++ app/src/main/java/com/example/shalom/androidlifecyclescodelab/ChronoActivity.java
@@ -1,5 +1,8 @@
 package com.example.shalom.androidlifecyclescodelab;
 
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Chronometer;
@@ -11,8 +14,25 @@ public class ChronoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chrono);
 
+        /*{@code ViewModelStore} provides a new ViewModel or one that was previously created*/
+        ChronometerViewModel viewModel = ViewModelProviders.of(this).get(ChronometerViewModel.class);
+
+        /*Get reference to {@code id/chronometer}*/
         Chronometer chronometer = (Chronometer) findViewById(R.id.chronometer);
 
+        if(viewModel.getStartTime() == null) {
+            /*When start time is not set*/
+            long startTime = SystemClock.elapsedRealtime();
+            viewModel.init(startTime);
+            chronometer.setBase(viewModel.getStartTime());
+        } else {
+            /*Viewmodel was retained after setting the start time, so set Chronometer's base to
+            * viewModel's {@code startTime}*/
+            long startTime = viewModel.getStartTime();
+            chronometer.setBase(startTime);
+        }
+
         chronometer.start();
+
     }
 }
