8439f54971a0f26bb10720184c190cd09e3ac663
Shalom Halbert <shalomhalbert@gmail.com>
2018-05-02 12:17:41 -0400

    Refactored name; Integrated LiveData
    - Refactored name to reflect use of Timer instead of Chronometer
    - Integrated LiveData for Observation in ChronoActivity
    - LiveData is updated every second, as is defined in the Timer instance

---
+++ app/src/main/java/com/example/shalom/androidlifecyclescodelab/LiveDataTimerViewModel.java
@@ -0,0 +1,42 @@
+package com.example.shalom.androidlifecyclescodelab;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+import android.os.SystemClock;
+import android.support.annotation.Nullable;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/*ViewModel holding data related to {@class ChronoActivity.java}*/
+public class LiveDataTimerViewModel extends ViewModel {
+
+    private static final int ONE_SECOND = 1000;
+
+    /*Value that will be observed*/
+    private MutableLiveData<Long> elapsedTime = new MutableLiveData<>();
+
+    private Long initialTime;
+
+    public LiveDataTimerViewModel() {
+        initialTime = SystemClock.elapsedRealtime();
+        Timer timer = new Timer();
+
+        /*Set frequency of data updates*/
+        timer.scheduleAtFixedRate(new TimerTask() {
+            /*Task to be run*/
+            @Override
+            public void run() {
+                final long newValue = (SystemClock.elapsedRealtime() - initialTime) / 1000;
+                /*Updated {@code elapsedTime}*/
+                elapsedTime.postValue(newValue);
+            }
+        }, ONE_SECOND, ONE_SECOND);
+    }
+
+    public LiveData<Long> getElapsedTime() {
+        return elapsedTime;
+    }
+
+}
