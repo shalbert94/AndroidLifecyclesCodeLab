e0b03ebfb3d5adc1f7ae71ed068fcf61604286b6
Shalom Halbert <shalomhalbert@gmail.com>
2018-05-02 12:22:22 -0400

    Rm'd Chronometer related code; Attached Observer to LiveDataViewModel
    - Effected by LiveDataViewModel name refactoring
    - Made viewModel globally accessible
    - Rm'd code relating to Chronometer
    - Created and Observer and attached it to LiveData in LiveDataViewModel
    which causes the textview to be updated every second

--- app/src/main/java/com/example/shalom/androidlifecyclescodelab/ChronoActivity.java
+++ app/src/main/java/com/example/shalom/androidlifecyclescodelab/ChronoActivity.java
@@ -1,13 +1,17 @@
 package com.example.shalom.androidlifecyclescodelab;
 
-import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
-import android.os.SystemClock;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Chronometer;
+import android.util.Log;
+import android.widget.TextView;
 
 public class ChronoActivity extends AppCompatActivity {
+    public static final String LOG_TAG = ChronoActivity.class.getSimpleName();
+
+    private LiveDataTimerViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,24 +19,32 @@ public class ChronoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chrono);
 
         /*{@code ViewModelStore} provides a new ViewModel or one that was previously created*/
-        ChronometerViewModel viewModel = ViewModelProviders.of(this).get(ChronometerViewModel.class);
+        viewModel = ViewModelProviders.of(this).get(LiveDataTimerViewModel.class);
 
-        /*Get reference to {@code id/chronometer}*/
-        Chronometer chronometer = (Chronometer) findViewById(R.id.chronometer);
+        subscribe();
+    }
 
-        if(viewModel.getStartTime() == null) {
-            /*When start time is not set*/
-            long startTime = SystemClock.elapsedRealtime();
-            viewModel.init(startTime);
-            chronometer.setBase(viewModel.getStartTime());
-        } else {
-            /*Viewmodel was retained after setting the start time, so set Chronometer's base to
-            * viewModel's {@code startTime}*/
-            long startTime = viewModel.getStartTime();
-            chronometer.setBase(startTime);
-        }
+    /**
+     * Attaches {@code elapsedTimeObserver} to {@code viewModel} to update R.id.timer_textview's text
+     * every second
+     */
+    private void subscribe() {
+        /*Observes changes to {@code viewModel.getElapsedTime()} and makes relevant changes in onChanged() */
+        final Observer<Long> elapsedTimeObserver = new Observer<Long>() {
+            /**
+             * Handles changes to the observed LiveData
+             * @param aLong the value that was changed
+             */
+            @Override
+            public void onChanged(@Nullable Long aLong) {
+                /*Updated text for R.id.timer_textview*/
+                String newText = ChronoActivity.this.getResources().getString(R.string.seconds, aLong);
+                ((TextView) findViewById(R.id.timer_textview)).setText(newText);
+                Log.d(LOG_TAG, "Updating timer");
+            }
+        };
 
-        chronometer.start();
-
+        /*Enable {@code elapsedTimeObserver} to observe a specific value in {@code viewModel}*/
+        viewModel.getElapsedTime().observe(this, elapsedTimeObserver);
     }
 }
